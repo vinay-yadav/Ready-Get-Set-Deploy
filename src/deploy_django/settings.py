9f222c0c6c551a90f44de71266514e4af68494e4
Vinay <yadav.vinay1508@gmail.com>
2025-05-05 23:29:12 +0530

    Updated ALLOWED_HOSTS

--- src/deploy_django/settings.py
+++ src/deploy_django/settings.py
@@ -30,11 +30,12 @@ SECRET_KEY = config("DJANGO_SECRET_KEY", default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost",
-    "http://127.0.0.1",
+    "https://*.railway.app",
 ]
 
 # Application definition
