21f85bbee8b51f35a3d8afa66711391cdaced176
Vinay <yadav.vinay1508@gmail.com>
2025-05-09 23:30:01 +0530

    Updated docker-run.sh

--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -7,8 +7,8 @@ python manage.py sendtestemail --admins
 python manage.py migrate --no-input
 python manage.py auto_admin
 
-RUNTIME_PORT = ${PORT:-8080}
-RUNTIME_HOST = ${HOST:-0.0.0.0}
+RUNTIME_PORT=${PORT:-8080}
+RUNTIME_HOST=${HOST:-0.0.0.0}
 
 # Add static files to container itself on runtime
 #python manage.py collectstatic --no-input
