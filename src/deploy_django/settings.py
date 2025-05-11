d1bd73d2e7b11096a6b4f5be11b2897677728145
Vinay <yadav.vinay1508@gmail.com>
2025-05-11 22:18:11 +0530

    Added pre-commit

--- src/deploy_django/settings.py
+++ src/deploy_django/settings.py
@@ -38,12 +38,10 @@ EMAIL_HOST = config("EMAIL_HOST", cast=str, default=None)
 EMAIL_PORT = config("EMAIL_PORT", cast=str, default="587")  # Recommended
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
-EMAIL_USE_TLS = config(
-    "EMAIL_USE_TLS", cast=bool, default=True
-)  # Use EMAIL_PORT 587 for TLS
-EMAIL_USE_SSL = config(
-    "EMAIL_USE_SSL", cast=bool, default=False
-)  # EUse MAIL_PORT 465 for SSL
+# Use EMAIL_PORT 587 for TLS
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
+# EUse MAIL_PORT 465 for SSL
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
